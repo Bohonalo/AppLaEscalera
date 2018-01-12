21622a3669da838e469f153306b04a1ebba3ee20
Juanra <bohonalo@gmail.com>
2018-01-12 11:17:40 +0100

    javier

--- app/src/main/java/com/example/a21639999/applaescalera/SplashScreen.java
+++ app/src/main/java/com/example/a21639999/applaescalera/SplashScreen.java
@@ -3,6 +3,9 @@ package com.example.a21639999.applaescalera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author 21639999 
+ */
 public class SplashScreen extends AppCompatActivity {
 
     @Override
