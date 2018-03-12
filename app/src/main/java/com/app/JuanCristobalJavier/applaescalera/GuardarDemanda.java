2ffb945eb1ad4445410d98e2fedcb1d776916d85
Bohonalo <bohonalo@gmail.com>
2018-03-12 13:19:05 +0100

    1.2

--- app/src/main/java/com/app/JuanCristobalJavier/applaescalera/GuardarDemanda.java
+++ app/src/main/java/com/app/JuanCristobalJavier/applaescalera/GuardarDemanda.java
@@ -2,29 +2,38 @@ package com.app.JuanCristobalJavier.applaescalera;
 
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.app.JuanCristobalJavier.applaescalera.model.Oferta;
+import com.app.JuanCristobalJavier.applaescalera.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GuardarDemanda extends Fragment {
 
-    private FirebaseFirestore db;
     private EditText nombre;
     private EditText descrip;
     private Button btnGuardar;
+    private Usuario u;
+    private String email;
+    private FirebaseDatabase fr;
+    private String nombreU;
+    private DatabaseReference dr;
 
 
     public GuardarDemanda() {
@@ -38,22 +47,67 @@ public class GuardarDemanda extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_guardar_demanda, container, false);
 
-        db = FirebaseFirestore.getInstance();
-        nombre = v.findViewById(R.id.edtNombreDemanda);
-        descrip = v.findViewById(R.id.edtDescripDemanda);
-        btnGuardar = v.findViewById(R.id.btnGuardarDemanda);
+        fr = FirebaseDatabase.getInstance();
 
-        btnGuardar.setOnClickListener(new View.OnClickListener(){
+        nombre = v.findViewById(R.id.edtNombreOferta);
+        descrip = v.findViewById(R.id.edtDescripOferta);
+        btnGuardar = v.findViewById(R.id.btnGuardarOferta);
+        u = new Usuario();
 
-            @Override
-            public void onClick(View v) {
+        email = obterEmail();
 
-
-
-            }
-        });
+        obterNombre();
+        
 
         return v;
     }
 
+
+    private void obterNombre() {
+        dr = fr.getReference("Usuarios");
+        dr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    u = dataSnapshot1.getValue(Usuario.class);
+                    System.out.print(u.getNombre());
+                    if (u.getEmail().equalsIgnoreCase(email)) {
+                        nombreU = u.getNombre();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String obterEmail() {
+        String email = "";
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            email = user.getEmail();
+        }
+        return email;
+    }
+
+    private void guardarObjetoRealtime(String emailP, String nombrePer) {
+        dr = FirebaseDatabase.getInstance().getReference().child("ObjetosOferta");
+
+        String descripO = descrip.getText().toString();
+        String nombreP = nombre.getText().toString();
+
+        Oferta o = new Oferta(nombreP, descripO, nombrePer, emailP);
+        dr.push().setValue(o);
+
+        ContenedorFragment nuevoFragmento = new ContenedorFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frament, nuevoFragmento);
+        transaction.addToBackStack(null);
+
+        // Commit a la transacción
+        transaction.commit();
+    }
 }
--- app/src/main/java/com/app/JuanCristobalJavier/applaescalera/GuardarOferta.java
+++ app/src/main/java/com/app/JuanCristobalJavier/applaescalera/GuardarOferta.java
@@ -1,9 +1,7 @@
 package com.app.JuanCristobalJavier.applaescalera;
 
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -11,14 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.app.JuanCristobalJavier.applaescalera.model.Oferta;
 import com.app.JuanCristobalJavier.applaescalera.model.Usuario;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,28 +19,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GuardarOferta extends Fragment {
-    private FirebaseFirestore db;
+
     private EditText nombre;
     private EditText descrip;
     private Button btnGuardar;
-    private DatabaseReference mDatabaseReference;
     private Usuario u;
+    private String email;
+    private FirebaseDatabase fr;
     private String nombreU;
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
+    private DatabaseReference dr;
 
     public GuardarOferta() {
         // Required empty public constructor
@@ -59,42 +45,40 @@ public class GuardarOferta extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_guardar_oferta, container, false);
+        fr = FirebaseDatabase.getInstance();
 
-        db = FirebaseFirestore.getInstance();
         nombre = v.findViewById(R.id.edtNombreOferta);
         descrip = v.findViewById(R.id.edtDescripOferta);
         btnGuardar = v.findViewById(R.id.btnGuardarOferta);
         u = new Usuario();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference =  firebaseDatabase.getReference("Usuarios");
+
+        email = obterEmail();
+
+        obterNombre();
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String email = obterEmail();
-
-                String nombrePersona = obterNombre(email);
-
-                guardarObjetoRealtime(email, nombrePersona);
-
+                guardarObjetoRealtime(email, nombreU);
             }
         });
 
         return v;
     }
 
-    private String obterNombre(final String email) {
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+    private void obterNombre() {
+        dr = fr.getReference("Usuarios");
+        dr.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
                     u = dataSnapshot1.getValue(Usuario.class);
                     System.out.print(u.getNombre());
-/*                    if (u.getEmail().equalsIgnoreCase(email)){
-                       nombreU =  u.getNombre();
-                    }*/
+                    if (u.getEmail().equalsIgnoreCase(email)) {
+                        nombreU = u.getNombre();
+                    }
                 }
             }
 
@@ -103,7 +87,6 @@ public class GuardarOferta extends Fragment {
 
             }
         });
-        return nombreU;
     }
 
     private String obterEmail() {
@@ -112,17 +95,17 @@ public class GuardarOferta extends Fragment {
         if (user != null) {
             email = user.getEmail();
         }
-        return  email;
+        return email;
     }
 
     private void guardarObjetoRealtime(String emailP, String nombrePer) {
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("ObjetosOferta");
+        dr = FirebaseDatabase.getInstance().getReference().child("ObjetosOferta");
 
         String descripO = descrip.getText().toString();
         String nombreP = nombre.getText().toString();
 
         Oferta o = new Oferta(nombreP, descripO, nombrePer, emailP);
-        mDatabaseReference.push().setValue(o);
+        dr.push().setValue(o);
 
         ContenedorFragment nuevoFragmento = new ContenedorFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
@@ -132,57 +115,4 @@ public class GuardarOferta extends Fragment {
         // Commit a la transacción
         transaction.commit();
     }
-
-    private void guardarObjetosCloud() {
-        String nombreO = nombre.getText().toString();
-        String descripO = descrip.getText().toString();
-
-        // Create a new user with a first and last name
-        Map<String, Object> ObjetosOfera = new HashMap<>();
-
-
-        //ObjetosOfera.put("NombrePersona", nombrePer);
-        ObjetosOfera.put("NombreObjeto", nombreO);
-        ObjetosOfera.put("Descripcion", descripO);
-
-        // Add a new document with a generated ID
-        db.collection("objetosOfeta")
-                .add(ObjetosOfera)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(getActivity(),"DocumentSnapshot added with ID: " + documentReference.getId(), Toast.LENGTH_LONG).show();
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getActivity(),"Error adding document", Toast.LENGTH_LONG).show();
-                    }
-                });
-    }
-
-
-    private void obtenerNombre() {
-
-        db.collection("Usuarios")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (DocumentSnapshot document : task.getResult()) {
-                                Map<String, Object> documento = document.getData();
-
-                                String nombreP = (String) documento.get("Nombre");
-
-                            }
-                        } else {
-                            Toast.makeText(getActivity(),"Error getting documents.", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-    }
-
 }
